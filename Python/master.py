e1dcc413a9e750e9e0c82bf360e7b30b3ef466c5
Magnus Alvestad <magnus.alvestad@gmail.com>
2017-10-31 22:42:22 +0100

    Changes

--- Python/master.py
+++ Python/master.py
@@ -79,8 +79,8 @@ def stateCommands(msgs):
         else:
                 if __debug__:
                         print("generator off");
-                [msg.send("light_on", 2) for msg in msgs]
-                [msg.send("light_off", 3) for msg in msgs]
+                [msg.send("light_on", 3) for msg in msgs]
+                [msg.send("light_off", 2) for msg in msgs]
         
         if (ac_on):
                 [msg.send("engage_dc_volt") for msg in msgs]
