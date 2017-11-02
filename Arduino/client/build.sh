cde982531754a068c98531ad642e33e86461a46a
Magnus Alvestad <magnus.alvestad@gmail.com>
2017-11-02 21:37:26 +0000

    Changes

--- Arduino/client/build.sh
+++ Arduino/client/build.sh
@@ -1,5 +1,4 @@
 git pull origin master
-ino clean && ino build --cppflags="-ffunction-sections -fdata-sections -g -Os -w -DBUTTON_ARDUINO" && ino upload -p /dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_85632313039351D0F020-if00
-ino clean && ino build --cppflags="-ffunction-sections -fdata-sections -g -Os -w -DCONTROLS_ARDUINO" && ino upload -p /dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_5573730303035141F012-if00
-
+ino clean && ino build --cppflags="-ffunction-sections -fdata-sections -g -Os -w -DBUTTON_ARDUINO" -m uno && ino upload -p /dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_85632313039351D0F020-if00 -m uno
+ino clean && ino build --cppflags="-ffunction-sections -fdata-sections -g -Os -w -DCONTROLS_ARDUINO" -m uno && ino upload -p /dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_5573730303035141F012-if00 -m uno
 
