c38dfc78e725df7242264f920870058339e303e3
Magnus Alvestad <magnus.alvestad@gmail.com>
2017-11-03 16:39:36 +0100

    Changes

--- Arduino/client/build.sh
+++ Arduino/client/build.sh
@@ -1,4 +1,12 @@
 git pull origin master
+
+# Buttons
 ino clean && ino build --cppflags="-ffunction-sections -fdata-sections -g -Os -w -DBUTTON_ARDUINO" -m uno && ino upload -p /dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_85632313039351D0F020-if00 -m uno
+
+# Controls
 ino clean && ino build --cppflags="-ffunction-sections -fdata-sections -g -Os -w -DCONTROLS_ARDUINO" -m uno && ino upload -p /dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_5573730303035141F012-if00 -m uno
+
+# VFD, Lights, DC Volt, DC Amp
+ino clean && ino build --cppflags="-ffunction-sections -fdata-sections -g -Os -w -DVFD_ARDUINO -DLIGHT_ARDUINO -DDC_VOLT_ARDUINO -DDC_AMP_ARDUINO" -m uno && ino upload -p /dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_55737303030351D052F0-if00 -m uno
+
 
