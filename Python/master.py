033a2200a755c8574d2f81f291e8e254de7173f3
Magnus Alvestad <magnus.alvestad@gmail.com>
2017-11-04 04:06:07 +0100

    Changes

--- Python/master.py
+++ Python/master.py
@@ -63,24 +63,24 @@ def stateCommands(msgs):
         if (transformer_on):
                 if __debug__:
                         print("transformer on");
-                [msg.send("light_on", 2) for msg in msgs]
-                [msg.send("light_off", 1) for msg in msgs]
+                [msg.send("light_on", 0) for msg in msgs]
+                [msg.send("light_off", 3) for msg in msgs]
         else:
                 if __debug__:
                         print("transformer off");
-                [msg.send("light_on", 1) for msg in msgs]
-                [msg.send("light_off", 2) for msg in msgs] 
+                [msg.send("light_on", 3) for msg in msgs]
+                [msg.send("light_off", 0) for msg in msgs] 
                 
         if (generator_on):
                 if __debug__:
                         print("generator on");
-                [msg.send("light_on", 0) for msg in msgs] 
-                [msg.send("light_off", 3) for msg in msgs]
+                [msg.send("light_on", 2) for msg in msgs] 
+                [msg.send("light_off", 1) for msg in msgs]
         else:
                 if __debug__:
                         print("generator off");
-                [msg.send("light_on", 3) for msg in msgs]
-                [msg.send("light_off", 0) for msg in msgs] 
+                [msg.send("light_on", 1) for msg in msgs]
+                [msg.send("light_off", 2) for msg in msgs] 
         
         if (dc_on):
                 [msg.send("engage_dc_volt") for msg in msgs]
