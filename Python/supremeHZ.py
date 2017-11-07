f6a06010fba32bac4a4a7f456c98a3bd3649775e
Magnus Alvestad <magnus.alvestad@gmail.com>
2017-11-07 22:33:08 +0100

    Changes

--- Python/supremeHZ.py
+++ Python/supremeHZ.py
@@ -8,7 +8,7 @@ from array import array
 from pygame.mixer import Sound, get_init, pre_init
 import random
 
-SILLY = False
+SILLY = True
 bits = 16
 #the number of channels specified here is NOT 
 #the channels talked about here http://www.pygame.org/docs/ref/mixer.html#pygame.mixer.get_num_channels
@@ -34,10 +34,10 @@ class Note(Sound):
 pygame.mixer.init(frequency=44100, size=-bits, channels=1)
 pygame.init()
 
+playingOrig = True
+
 duration = 3.0          # in seconds
-freqs=[50.0]
 # freqs = [45.0, 46.0, 47.0, 48.0, 49.0, 50.0, 51.0, 52.0, 53.0, 54.0, 55.0]
-sounds = []
 freq = 52.5
 
 #this sounds totally different coming out of a laptop versus coming out of headphones
@@ -47,16 +47,8 @@ n_samples = int(round(duration*sample_rate))
 
 #setup our numpy array to handle 16 bit ints, which is what we set our mixer to expect with "bits" up above
 
-for frequency in freqs:
-
-  n = Note(frequency, 0.0)
-  print("generating %f", frequency)  
-
-  n.play(loops = -1)
-  n.set_volume(0.0)
-  sounds.append(n)
-
-print(sounds)
+n = Note(freq, 1.0)
+n.play(loops = -1)
 
 if (not SILLY):
   port = 5556
@@ -68,33 +60,21 @@ if (not SILLY):
 while True:
   if (not SILLY):
     socket.send_string("get_hz_raw")
-    freq = float(socket.recv())
-  print(freq)
-
-
-# Silence the nonsense
-  for s in sounds:
-    print("Snd: %s Vol: %f" % (s, s.get_volume()))
-    s.set_volume(0.0)
-
-##  if (freq >= 45 and freq <= 55):
-##    print ("Turning up %d" % (freq-45))
-##    sounds[freq-45].set_volume(1.0)
-
-    print("f: %f" % freq)
-
-    sounds[0].set_volume(1.0)
-    s = pygame.sndarray.samples(sounds[0])
-#    nnn = pygame.sndarray.samples(Note(freq, 0.0))
-
-#    print("s: %d nnn: %d s[100]: %d" % (np.size(s),np.size(nnn),s[100]))
-    
-#    s[:] = np.concatenate((nnn,nnn))[:np.size(s)]
-    
-    Note(freq, 0.0).play()
-
-    if SILLY: freq = freq + random.random() - 0.5
-
-    sleep(3)
+    freqNew = float(socket.recv())
+  if SILLY: freqNew = freq + random.random() - 0.5
+  print("f: %f" % freqNew)
+  if (freqNew != freq):
+    if (playingOrig):
+      n2 = Note(freqNew, 1.0)
+      n2.play(loops = -1, fade_ms=1000)
+      n.fadeout(1000);
+    else:
+      n = Note(freqNew, 1.0)
+      n.play(loops = -1, fade_ms=1000)
+      n2.fadeout(1000);
+    freqNew = freq;
+    playingOrig = not playingOrig
+  print("dibadoo")
+  sleep(3)
 
 
